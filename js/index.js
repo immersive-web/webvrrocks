0835dc29290d6fc025d2369d4d23c945cc20956a
Christopher Van <cvan@mozilla.com>
2016-11-08 05:07:10 -0800

    add GA (fixes #23)

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
+/* global ga */
 var domready = require('domready');
 // require('delayed-scroll-restoration-polyfill');
+
+/* jshint ignore:start */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){  // eslint-disable-line
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),    // eslint-disable-line
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)  // eslint-disable-line
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');  // eslint-disable-line
+/* jshint ignore:end */
+
+ga('create', 'UA-86987247-1', 'auto');
+ga('send', 'pageview');
 
 require('./vendor/pjax-standalone');  // NOTE: module does not support CommonJS atm.
 
