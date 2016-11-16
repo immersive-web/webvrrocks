07c4421dae96fa35f92aabca69c4231f17ecf266
Christopher Van <cvan@mozilla.com>
2016-11-16 13:07:22 -0800

    create unique style for homepage

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 /* global ga */
 var domready = require('domready');
-// require('delayed-scroll-restoration-polyfill');
 
 /* jshint ignore:start */
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){  // eslint-disable-line
@@ -11,8 +10,6 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) 
 
 ga('create', 'UA-86987247-1', 'auto');
 ga('send', 'pageview');
-
-require('./vendor/pjax-standalone');  // NOTE: module does not support CommonJS atm.
 
 domready(() => {
   var html = document.documentElement;
@@ -26,36 +23,10 @@ domready(() => {
 
   // TODO: Debounce.
   window.addEventListener('resize', () => {
-// - document.querySelector('#nav-footer').offsetHeight
-
     if (nav.scrollHeight > nav.clientHeight) {
       nav.classList.add('elastic');
     } else {
       nav.classList.remove('elastic');
     }
-
   });
-
-  // window.pjax.connect({
-  //   container: 'main',
-  //   returnToTop: true,
-  //   excludeClass: 'nav-toggle-anchor',
-  //   complete: () => {
-  //     var elWithPagePath = document.querySelector('[data-current-file]');
-  //     if (elWithPagePath) {
-  //       var editPageLink = document.querySelector('#edit-page-link');
-  //       if (editPageLink) {
-  //         editPageLink.setAttribute('href', editPageLink.getAttribute('data-default-href').replace('{path}', elWithPagePath.getAttribute('data-current-file')));
-  //       }
-  //     }
-  //   },
-  //   success: () => {
-  //     html.setAttribute('data-nav-open', false);
-  //   },
-  //   error: event => {
-  //     console.error(event);
-  //     console.log('Fallback to synchronous page load', event.data.url);
-  //     window.location.href = event.data.url;
-  //   }
-  // });
 });
